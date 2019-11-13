682b7c8fe2af47eae3e08ec82f75b5811b5d31e6
FIC <FIC@info.local>
2019-11-13 12:20:35 -0200

    Gufos_react_js

---
+++ src/assets/componentes/Footer/Footer.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Footer() {
+  return (
+   <footer>Escola Senai de Informatica</footer>
+  );
+}
+
+export default Footer;
---
+++ src/assets/pages/Categoria/Categoria.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import Footer from  '../../componentes/Footer/Footer';
+
+class Categoria extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Categoria</h1>
+                <Footer/>
+            </div>
+        );
+    }
+}
+
+export default Categoria;
